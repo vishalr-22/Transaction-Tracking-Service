85a16ffec881aa4c783213632f4535c42cca64b7
Dheepa Ramanathan <vishalramanathan22@gmail.com>
2023-05-15 09:44:43 +0530

    model created

--- src/main/java/com/example/transaction/model/Transaction.java
+++ src/main/java/com/example/transaction/model/Transaction.java
@@ -1,5 +1,80 @@
 package com.example.transaction.model;
 
-public class Transaction {
-    
+import java.io.Serializable;
+import java.sql.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Transaction")
+public class Transaction implements Serializable{
+    @Column(name = "transactionId")
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long transactionId;
+
+    @Column(name = "trans_type")
+    private Integer transType = null;
+
+    @Column(name = "name")
+    private String name=null;
+
+    @Column(name = "entry_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date entryDate;
+
+    @Column(name = "amount")
+    private Integer amount = null;
+
+    public Transaction() {
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", transType=" + transType +
+                ", entryDate=" + entryDate +
+                ", amount=" + amount +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    public Transaction( Integer transType, String name, Date entryDate, Integer amount) {
+        this.transType = transType;
+        this.amount = amount;
+        this.entryDate = entryDate;
+        this.name = name;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
